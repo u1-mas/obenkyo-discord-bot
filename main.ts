ba89c8ecad5448c612320ac269bce5aa220820c8
u1_mas <mtakuya1022@gmail.com>
2024-07-09 22:47:34 +0900

    add ping

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { createBot, Intents } from "@discordeno/bot";
+import { createBot, Intents, InteractionResponseTypes } from "@discordeno/bot";
 import { load } from "@std/dotenv";
 
 const env = await load();
@@ -6,7 +6,7 @@ const token = env.DISCORD_BOT_TOKEN;
 if (token === undefined) {
   throw new Error("DISCORD_BOT_TOKEN is not found.");
 }
-export const bot = createBot({
+const bot = createBot({
   token: token,
   intents: Intents.Guilds | Intents.GuildMessages | Intents.MessageContent,
   events: {
@@ -16,10 +16,38 @@ export const bot = createBot({
   },
 });
 
+bot.transformers.desiredProperties.interaction.id = true;
+bot.transformers.desiredProperties.interaction.data = true;
+bot.transformers.desiredProperties.interaction.type = true;
+bot.transformers.desiredProperties.interaction.token = true;
+bot.transformers.desiredProperties.interaction.channelId = true;
+
 bot.transformers.desiredProperties.message.content = true;
 
+bot.transformers.desiredProperties.role.id = true;
+
 bot.events.messageCreate = (msg) => {
-  console.log(msg.content);
+  console.log("content: ", msg.content);
+};
+
+await bot.rest.createGlobalApplicationCommand({
+  name: "ping",
+  description: "ping!",
+});
+bot.events.interactionCreate = async (interaction) => {
+  console.log("interaction name: ", interaction.data?.name);
+  if (interaction.data?.name === "ping") {
+    return await bot.helpers.sendInteractionResponse(
+      interaction.id,
+      interaction.token,
+      {
+        type: InteractionResponseTypes.ChannelMessageWithSource,
+        data: {
+          content: "pong!",
+        },
+      },
+    );
+  }
 };
 
 await bot.start();
